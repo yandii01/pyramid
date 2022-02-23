c6e8c914ce88cc1460cd573de2ff0ddfcce04395
yandii01 <yandip123@gmail.com>
2022-02-23 11:32:38 +0700

    update

--- pyramid.c
+++ pyramid.c
@@ -5,14 +5,17 @@ int main() {
 	scanf("%d", &a);
 	if(a>=1){
 		for (i = 0; i < a; i++) {
-			for (b = 1; b <= a - i; b++)
+			for (b = 1; b <= a - i; b++){
 				printf("  ");
+			}
 			for (j = 0; j <= i; j++) {
-				if (j == 0 || i == 0)
+				if (j == 0 ){
 					c = 1;
-				else
+				}
+				else{
 					c = c * (i - j + 1) / j;
 					printf("%4d", c);
+				}
 			}
 			printf("\n");
 		}
@@ -20,11 +23,13 @@ int main() {
 	else{
 	    a = abs(a);
 		for (i = 0; i < a; i++) {
-			for (b = 0; b < i; b++)
+			for (b = 0; b < i; b++){
 				printf("  ");
+			}
 			for (j = 0; j < a - i; j++) {
-				if (j == 0 )
+				if (j == 0 ){
 					c = 1;
+				}
 				else
 					c = c * (a - j - i) / j;
 					printf("%4d", c);
